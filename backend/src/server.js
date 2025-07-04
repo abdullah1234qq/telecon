cd00a1bcea2d7f4447d6a1ee837b1f0f2be98106
Abdullah Hussain <abdullah1234qq>
2025-07-04 17:11:20 +0500

    db models created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,12 @@
 
 import express from 'express';
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 const app = express()
-app.listen(process.env.PORT, () => console.log("Server is Running"))
+connectDB()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(ENV.PORT, () => console.log("Server is Running", ENV.PORT))
+// teckdevme1
+// UKbjsGLIcQ2dt7SG
